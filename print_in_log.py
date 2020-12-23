3777e859088129edec783a8f9a9c6112f96d53e2
troywu666 <kpwcy@qq.com>
2020-12-23 09:39:33 +0800

    update Save_log

--- print_in_log.py
+++ print_in_log.py
@@ -4,11 +4,12 @@ Version: 1.0
 Autor: Troy Wu
 Date: 2020-02-14 12:26:50
 LastEditors: Troy Wu
-LastEditTime: 2020-12-19 14:41:32
+LastEditTime: 2020-12-23 09:38:48
 '''
 
 import logging
 import traceback
+from functools import reduce
 
 class Save_log():
 	def __init__(self, path, logfile_name):
@@ -16,20 +17,22 @@ class Save_log():
 		logger.setLevel(logging.INFO)
 
 		logfile = path + '/' + logfile_name + '.log'
-		fh = logging.FileHandler(logfile, mode = 'a')
+		fh = logging.FileHandler(logfile, mode = 'w')
 		fh.setLevel(logging.INFO)
 
 		ch = logging.StreamHandler()
 		ch.setLevel(logging.INFO)
 
-		formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+		# formatter = logging.Formatter("\n%(message)s")
+		formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: \n %(message)s")
 		fh.setFormatter(formatter)
 		ch.setFormatter(formatter)
 
 		logger.addHandler(fh)
 		logger.addHandler(ch)
 
-	def info(self, statement):
+	def info(self, *statement):
+		statement = reduce(lambda x, y: str(x)+'\t'+str(y)+'\t', statement)
 		logging.info(statement)
   
 	def error(self):
--- 验证.py
+++ 验证.py
@@ -4,79 +4,78 @@
 @Autor: Troy Wu
 @Date: 2020-02-12 16:44:46
 LastEditors: Troy Wu
-LastEditTime: 2020-12-21 01:23:17
+LastEditTime: 2020-12-22 19:39:53
 '''
 
 try:
-# 首次运行可能需要将那些包所在路径加入到环境变量当中
-import sys
-sys.path.append(r'D:\troywu666\business_stuff\民生对公项目\模型标准化')
-from evaluation import Metrics, Metrics_comparison
-from train_val import Model_training, bay_opt_lgb
-from model_io import Model_pickle
-from data_exploring import Explore
-from feature_selection import Selector, iv_filter
-from preprocessing import Transformer
-from predict import Prediction
-from print_in_log import Save_log
-from sklearn.datasets import load_breast_cancer
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-#import logging
-#import configparser
+    # 首次运行可能需要将那些包所在路径加入到环境变量当中
+    import sys
+    sys.path.append(r'D:\troywu666\business_stuff\民生对公项目\模型标准化')
+    from evaluation import Metrics, Metrics_comparison
+    from train_val import Model_training, bay_opt_lgb
+    from model_io import Model_pickle
+    from data_exploring import Explore
+    from feature_selection import Selector, iv_filter
+    from preprocessing import Transformer
+    from predict import Prediction
+    from print_in_log import Save_log
+    from sklearn.datasets import load_breast_cancer
+    import pandas as pd
+    import numpy as np
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.ensemble import RandomForestClassifier
+    #import logging
+    #import configparser
 
-'''config = configparser.ConfigParser()
-config['server'] = {'user': 'dbc', 'password': 'pass', 'host': '192.168.253.231', 'port': '22', 'dbname': 'server1'}
-with open(r'D:/troywu666/business_stuff/民生对公项目/模型标准化/configuration.ini', 'w') as f:
-    config.write(f)'''
+    '''config = configparser.ConfigParser()
+    config['server'] = {'user': 'dbc', 'password': 'pass', 'host': '192.168.253.231', 'port': '22', 'dbname': 'server1'}
+    with open(r'D:/troywu666/business_stuff/民生对公项目/模型标准化/configuration.ini', 'w') as f:
+        config.write(f)'''
 
-# 载入数据集
-data = load_breast_cancer()
-df = pd.DataFrame(data.data, columns = data.feature_names)
-df.iloc[2, -1] = np.nan
+    # 载入数据集
+    data = load_breast_cancer()
+    df = pd.DataFrame(data.data, columns = data.feature_names)
+    df.iloc[2, -1] = np.nan
 
-# 实例化日志打印模块
-path = r'D:/troywu666/business_stuff/民生对公项目/模型标准化'
-save_log = Save_log(path, 'test')
+    # 实例化日志打印模块
+    path = r'D:/troywu666/business_stuff/民生对公项目/模型标准化'
+    save_log = Save_log(path, 'test')
 
-path = r'D:/troywu666/business_stuff/民生对公项目/模型标准化'
-    
+    path = r'D:/troywu666/business_stuff/民生对公项目/模型标准化'
+        
 
-'''lr = LogisticRegression()
-lr.fit(data.data, data.target)
-y_pred = lr.predict_proba(data.data)[:, 1]
-y_pred = {'rf': y_pred, "lr": y_pred}
-'''
-# 数据探索
-num_des = Explore(df).describe_num()
-Explore(df).corr_and_plot()
-Explore(df).distplot()
-#Explore(df).pairplot()
+    '''lr = LogisticRegression()
+    lr.fit(data.data, data.target)
+    y_pred = lr.predict_proba(data.data)[:, 1]
+    y_pred = {'rf': y_pred, "lr": y_pred}
+    '''
+    # 数据探索
+    num_des = Explore(df).describe_num()
+    Explore(df).corr_and_plot()
+    Explore(df).distplot()
+    #Explore(df).pairplot()
 
-save_log.info('Exploring complete.')
-#df.value_counts()
-save_log.info('DataFrame load complete.')
+    save_log.info('Exploring complete.')
+    #df.value_counts()
+    save_log.info(**{'a': 1, "b": 2})
 
 
+    # 数据预处理
+    #transformer, transformed_data = Transformer(df['mean radius']).box_cox()
+    transformer_1, transformed_data = Transformer(df).scaler('standard')
+    transformer, transformed_data = Transformer(transformed_data).fillna(fill_value = 2, strategy = 'constant')
+    #transformer, transformed_data = Transformer(transformed_data).encoder(method = 'binarizer', threshold = 3)
+    save_log.info('Data preprocessing complete.')
 
-# 数据预处理
-#transformer, transformed_data = Transformer(df['mean radius']).box_cox()
-transformer_1, transformed_data = Transformer(df).scaler('standard')
-transformer, transformed_data = Transformer(transformed_data).fillna(fill_value = 2, strategy = 'constant')
-#transformer, transformed_data = Transformer(transformed_data).encoder(method = 'binarizer', threshold = 3)
-save_log.info('Data preprocessing complete.')
+    # 特征选择
+    selector, selected_data = Selector(transformed_data).filter(data.target, k = 30, method = 'mutual_clas_filter')
+    selector, selected_data = Selector(transformed_data).wrapper(RandomForestClassifier(), data.target, n = 30, step = 10)
+    selector, selected_data = Selector(transformed_data).embedded(LogisticRegression(), data.target, threshold = 0.03)
+    selector, selected_data = Selector(transformed_data).variance(threshold = 0.001)
+    selector, selected_data = Selector(df).filter(data.target, k = 30, method = 'IV')
 
-# 特征选择
-selector, selected_data = Selector(transformed_data).filter(data.target, k = 30, method = 'mutual_clas_filter')
-selector, selected_data = Selector(transformed_data).wrapper(RandomForestClassifier(), data.target, n = 30, step = 10)
-selector, selected_data = Selector(transformed_data).embedded(LogisticRegression(), data.target, threshold = 0.03)
-selector, selected_data = Selector(transformed_data).variance(threshold = 0.001)
-selector, selected_data = Selector(df).filter(data.target, k = 30, method = 'IV')
-
-iv_filter(df, data.target)
-save_log.info('Feature selection complete.')
+    iv_filter(df, data.target)
+    save_log.info('Feature selection complete.')
 except:
     save_log.error()
 
